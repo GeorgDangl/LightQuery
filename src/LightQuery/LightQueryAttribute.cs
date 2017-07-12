b51ea063b36014776baed248d1bd235a4fb5af5f
Georg Dangl <georg@dangl.me>
2017-07-13 00:06:04 +0200

    Add LightQueryAttribute as WIP
    Unit tests are still missing

---
+++ src/LightQuery/LightQueryAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LightQuery
+{
+    public class LightQueryAttribute :  ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (context.HttpContext?.Request?.Query?.Any() != true)
+            {
+                return;
+            }
+            var queryOptions = QueryParser.GetQueryOptions(context.HttpContext.Request.Query);
+            if (string.IsNullOrWhiteSpace(queryOptions.SortPropertyName))
+            {
+                return;
+            }
+            var objectResult = context.Result as ObjectResult;
+            var queryable = objectResult?.Value as IQueryable;
+            if (queryable == null)
+            {
+                return;
+            }
+            var result = queryable.ApplySorting(queryOptions);
+            objectResult.Value = result;
+        }
+    }
+}
---
+++ test/LightQuery.Tests/LightQueryAttributeTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Xunit;
+
+namespace LightQuery.Tests
+{
+    public class LightQueryAttributeTests
+    {
+        private readonly LightQueryAttribute _attribute = new LightQueryAttribute();
+
+        [Fact]
+        public void ArgumentNullExceptionOnNullContext()
+        {
+            Assert.Throws<ArgumentNullException>("context", () => new LightQueryAttribute().OnResultExecuting(null));
+        }
+
+        [Fact]
+        public void DoNothingWhenNoQueryParameters()
+        {
+            var context = GetMockedContext();
+            Assert.False(context.HttpContext.Request.Query.Any());
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void DoNothingWhenNullQueryParameters()
+        {
+            var context = GetMockedContext();
+            context.HttpContext.Request.Query = null;
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void DoNothingWhenInvalidPropertyName()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void DoNothingWhenNoQueryableContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void CallQueryableProcessorOnValidQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        private ResultExecutingContext GetMockedContext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
