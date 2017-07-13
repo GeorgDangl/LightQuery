51830fe521d0b3c99615092291006096e097bb2d
Georg Dangl <georg@dangl.me>
2017-07-13 20:01:05 +0200

    Add tests and remove hard to mock ones
    They're being taken care of in the integration tests themselves and all I could really test anyway was if the LightQueryAttribute is making the proper calls

--- test/LightQuery.Tests/LightQueryAttributeTests.cs
+++ test/LightQuery.Tests/LightQueryAttributeTests.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Xunit;
 
 namespace LightQuery.Tests
@@ -16,45 +11,6 @@ namespace LightQuery.Tests
         public void ArgumentNullExceptionOnNullContext()
         {
             Assert.Throws<ArgumentNullException>("context", () => new LightQueryAttribute().OnResultExecuting(null));
-        }
-
-        [Fact]
-        public void DoNothingWhenNoQueryParameters()
-        {
-            var context = GetMockedContext();
-            Assert.False(context.HttpContext.Request.Query.Any());
-            throw new NotImplementedException();
-        }
-
-        [Fact]
-        public void DoNothingWhenNullQueryParameters()
-        {
-            var context = GetMockedContext();
-            context.HttpContext.Request.Query = null;
-            throw new NotImplementedException();
-        }
-
-        [Fact]
-        public void DoNothingWhenInvalidPropertyName()
-        {
-            throw new NotImplementedException();
-        }
-
-        [Fact]
-        public void DoNothingWhenNoQueryableContent()
-        {
-            throw new NotImplementedException();
-        }
-
-        [Fact]
-        public void CallQueryableProcessorOnValidQuery()
-        {
-            throw new NotImplementedException();
-        }
-
-        private ResultExecutingContext GetMockedContext()
-        {
-            throw new NotImplementedException();
         }
     }
 }
--- test/LightQuery.Tests/QueryParserTests.cs
+++ test/LightQuery.Tests/QueryParserTests.cs
@@ -103,5 +103,77 @@ namespace LightQuery.Tests
             ParseQuery();
             Assert.True(_result.IsDescending);
         }
+
+        [Fact]
+        public void ParsePage()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "page", new Microsoft.Extensions.Primitives.StringValues("12") } });
+            ParseQuery();
+            Assert.Equal(12, _result.Page);
+        }
+
+        [Fact]
+        public void UseFirstPageForZeroPage()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "page", new Microsoft.Extensions.Primitives.StringValues("0") } });
+            ParseQuery();
+            Assert.Equal(1, _result.Page);
+        }
+
+        [Fact]
+        public void UseFirstPageForNegativePage()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "page", new Microsoft.Extensions.Primitives.StringValues("-2") } });
+            ParseQuery();
+            Assert.Equal(1, _result.Page);
+        }
+
+        [Fact]
+        public void UseFirstPageForNaNPage()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "page", new Microsoft.Extensions.Primitives.StringValues("second") } });
+            ParseQuery();
+            Assert.Equal(1, _result.Page);
+        }
+
+        [Fact]
+        public void ParsePageSize()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "pageSize", new Microsoft.Extensions.Primitives.StringValues("20") } });
+            ParseQuery();
+            Assert.Equal(20, _result.PageSize);
+        }
+
+        [Fact]
+        public void ParsePageSizeBiggerThanDefault()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "pageSize", new Microsoft.Extensions.Primitives.StringValues("200") } });
+            ParseQuery();
+            Assert.Equal(200, _result.PageSize);
+        }
+
+        [Fact]
+        public void UseDefaultPageSizeForZeroPageSize()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "pageSize", new Microsoft.Extensions.Primitives.StringValues("0") } });
+            ParseQuery();
+            Assert.Equal(50, _result.PageSize);
+        }
+
+        [Fact]
+        public void UseDefaultPageSizeForNegativePageSize()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "pageSize", new Microsoft.Extensions.Primitives.StringValues("-3") } });
+            ParseQuery();
+            Assert.Equal(50, _result.PageSize);
+        }
+
+        [Fact]
+        public void UseDefaultPageSizeForNaNPageSize()
+        {
+            _query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues> { { "pageSize", new Microsoft.Extensions.Primitives.StringValues("twenty") } });
+            ParseQuery();
+            Assert.Equal(50, _result.PageSize);
+        }
     }
 }
