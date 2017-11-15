c6f99e45ff8eeb704afcefacd072c3fe29851e3d
Georg Dangl <georg@dangl.me>
2017-11-15 23:31:19 +0100

    Update test script

--- test.ps1
+++ test.ps1
@@ -1,17 +1,28 @@
 $testProjects = "LightQuery.Client.Tests", "LightQuery.Client.Tests.Integration", "LightQuery.EntityFrameworkCore.Tests", "LightQuery.EntityFrameworkCore.Tests.Integration", "LightQuery.Shared.Tests", "LightQuery.Tests", "LightQuery.Tests.Integration"
-$testFrameworks = "net461", "netcoreapp1.1", "netcoreapp2.0"
 $testRuns = 1;
 
 & dotnet restore
-
+& dotnet build -c Debug
+	
+$oldResults = Get-ChildItem -Path "$PSScriptRoot\results_*.testresults"
+if ($oldResults) {
+    Remove-Item $oldResults
+}
+	
 foreach ($testProject in $testProjects){
-    foreach ($testFramework in $testFrameworks){
     & cd "$PSScriptRoot\test\$testProject"
-        & dotnet.exe xunit -f $testFramework `
-            -xml "$PSScriptRoot\results_$testRuns.testresults"   
-			
+	
+    & dotnet.exe xunit `
+        -nobuild `
+        -parallel all `
+        -xml $PSScriptRoot\results_$testRuns.testresults   
+                 
         $testRuns++
-   }
 }
 
-& cd $PSScriptRoot
+& cd "$PSScriptRoot"
+
+"Prepending framework to test method name for better CI visualization"
+$resultsGlobPattern = "results_*.testresults"
+$prependFrameworkScript = ".\AppendxUnitFramework.ps1"
+& $prependFrameworkScript $resultsGlobPattern "$PSScriptRoot"
