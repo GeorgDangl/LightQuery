69f95217c8733f2af4fba7cd536ac4933440ec5a
Georg Dangl <georg@dangl.me>
2018-09-15 23:15:44 +0200

    Move class to its own file

---
+++ test/LightQuery.Client.Tests/MockPayload.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LightQuery.Client.Tests
+{
+    public class MockPayload
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+}
--- test/LightQuery.Client.Tests/PaginationBaseServiceTests.cs
+++ test/LightQuery.Client.Tests/PaginationBaseServiceTests.cs
@@ -12,13 +12,6 @@ using Xunit;
 
 namespace LightQuery.Client.Tests
 {
-    public class MockPayload
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public int Order { get; set; }
-    }
-    
     public class PaginationBaseServiceTests
     {
         [Fact]
