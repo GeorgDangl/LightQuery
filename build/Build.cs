3a54acd883344d24cb1ddb5d96868eaf98e743b3
Georg Dangl <georg@dangl.me>
2019-01-31 08:03:02 +0100

    Update build dependencies

--- build/Build.cs
+++ build/Build.cs
@@ -54,7 +54,7 @@ class Build : NukeBuild
     [GitVersion] readonly GitVersion GitVersion;
     [GitRepository] readonly GitRepository GitRepository;
 
-    [KeyVaultSecret] readonly string DocuApiEndpoint;
+    [KeyVaultSecret] readonly string DocuBaseUrl;
     [KeyVaultSecret] readonly string PublicMyGetSource;
     [KeyVaultSecret] readonly string PublicMyGetApiKey;
     [KeyVaultSecret] readonly string NuGetApiKey;
@@ -244,11 +244,11 @@ class Build : NukeBuild
         .DependsOn(Push) // To have a relation between pushed package version and published docs version
         .DependsOn(BuildDocumentation)
         .Requires(() => DocuApiKey)
-        .Requires(() => DocuApiEndpoint)
+        .Requires(() => DocuBaseUrl)
         .Executes(() =>
         {
             WebDocu(s => s
-                .SetDocuApiEndpoint(DocuApiEndpoint)
+                .SetDocuBaseUrl(DocuBaseUrl)
                 .SetDocuApiKey(DocuApiKey)
                 .SetSourceDirectory(OutputDirectory / "docs")
                 .SetVersion(GitVersion.NuGetVersion)
